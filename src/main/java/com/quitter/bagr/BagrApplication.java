3677c73c0daa405b8579416c28d0d6e892afb7a4
Anshika <anshikamsr@gmail.com>
2022-12-21 04:05:32 +0530

    initial commit

---
+++ src/main/java/com/quitter/bagr/BagrApplication.java
@@ -0,0 +1,13 @@
+package com.quitter.bagr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagrApplication.class, args);
+	}
+
+}
